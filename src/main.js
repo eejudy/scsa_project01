bc7921a96331598a685fe6702502b1e170b19619
ieeju <ieeju641023@gmail.com>
2023-05-05 13:25:12 +0900

    cors, axios 코드 추가

--- src/main.js
+++ src/main.js
@@ -2,9 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-
+import axios from 'axios'
 loadFonts()
 
+
+let url="http://127.0.0.1:8000/user/"
+
+axios.get(url)
+.then(function(response){
+  console.log(response);
+})
+.catch(function(response){
+  console.log(response);
+})
 createApp(App)
   .use(vuetify)
   .mount('#app')
