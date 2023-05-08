d5c63057cc10f05eebb19e40840d5bd14c788083
ieeju0102 <ieeju641023@gmail.com>
2023-05-08 15:32:11 +0900

    FRONT - home, intro, rank css 수정 BACK - User Model의 username에 unique 조건 추가

--- jango-project/boards/models.py
+++ jango-project/boards/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 # score로 변경
 class User(models.Model):
-    username = models.CharField(max_length=15)
+    username = models.CharField(max_length=15, unique=True)
     score = models.IntegerField()
     # city = models.CharField(max_length=15)
 
