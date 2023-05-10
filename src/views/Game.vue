5eb5af5d4fbeab2df0303e2b9fb0d4d4a1ec015f
ieeju <ieeju641023@gmail.com>
2023-05-10 16:20:22 +0900

    코드 정리

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,9 +1,14 @@
 <template>
   <v-container class="container">
     <div class="buttons">
-      <h2 v-if="targetScore<=score && score!=0">명예의 전당에 올랐습니다!</h2>
-      <h2 v-else>{{targetScore-score}}점 획득시 명예의 전당에 오를 수 있습니다!</h2>
+      <h2 v-if="targetScore <= score && score != 0">
+        명예의 전당에 올랐습니다!
+      </h2>
+      <h2 v-else>
+        {{ targetScore - score }}점 획득시 명예의 전당에 오를 수 있습니다!
+      </h2>
     </div>
+
     <!-- <v-btn
               @click="
                 play(
@@ -16,7 +21,6 @@
       <div class="card">
         <div class="card-body">
           <v-img
-            @click="select(item)"
             :src="require(`../assets/character/${img}.png`)"
             height="100"
           />
@@ -26,7 +30,7 @@
           <h3 align="center">{{ score }}점</h3>
         </div>
       </div>
-      
+
       <img
         class="mouse-img"
         id="intro"
@@ -66,10 +70,10 @@ import axios from "axios";
 export default {
   name: "Game",
   mounted() {},
-  created(){
-    const vm = this
-    vm.callF()
-    vm.getScore()
+  created() {
+    const vm = this;
+    vm.callF();
+    vm.getScore();
   },
   data: () => ({
     result: [],
@@ -83,15 +87,13 @@ export default {
     img: localStorage.getItem("img"),
   }),
   methods: {
-    getScore(){
+    getScore() {
       let url = "http://127.0.0.1:8000/min_score/";
-      const vm = this
-        axios
-          .get(url)
-          .then(function (response) {
-            vm.targetScore = response.data
-          })
-          console.log(vm.targetScore)
+      const vm = this;
+      axios.get(url).then(function (response) {
+        vm.targetScore = response.data;
+      });
+      console.log(vm.targetScore);
     },
     play(sound) {
       var audio = new Audio(sound);
@@ -102,65 +104,84 @@ export default {
       //닉네임 입력 화면으로 이동
       this.$router.push("/intro");
     },
-    callF(){
+    callF() {
       const vm = this;
-      let num = 3
-      vm.result.push(num)
-    },
-    getData() {
-      let url = "http://127.0.0.1:8000/result_test/";
-      const vm = this
+      let url = "http://127.0.0.1:8000/predict/";
       let param = {
-          num: 5
-        };
-        axios
-          .post(url, param)
-          .then(function (response) {
-            let num = response.data.num
-            vm.result.push(num)
-          })
-          .catch(function (response) {
-            console.log('fail')
-          });
+        start: true,
+      };
+      axios
+        .post(url, param)
+        .then(function (response) {
+          let num = response.data;
+          vm.result.push(num);
+        })
+        .catch(function (response) {
+          console.log("fail");
+        });
     },
-    save(){
+    getData(item) {
+      let url = "http://127.0.0.1:8000/predict/";
+      const vm = this;
+      let param = {
+        num: item,
+        start: false,
+      };
+      axios
+        .post(url, param)
+        .then(function (response) {
+          let num = response.data;
+          vm.result.push(num);
+        })
+        .catch(function (response) {
+          console.log("fail");
+        });
+    },
+    save() {
       let url = "http://127.0.0.1:8000/user/";
       const vm = this;
       let param = {
-        "username":vm.name,
-        "score":vm.score
-      }
+        username: vm.name,
+        score: vm.score,
+      };
       axios
-          .post(url, param)
-          .then(function (response) {
-            lconsole.log(response)
-          })
-          .catch(function (response) {
-            console.log('fail')
-          });
+        .post(url, param)
+        .then(function (response) {
+          lconsole.log(response);
+        })
+        .catch(function (response) {
+          console.log("fail");
+        });
     },
     goal(item) {
       const vm = this;
-      vm.getData()
+      vm.getData(item);
       let num = this.result.shift();
-      console.log(num)
+      console.log(num);
       if (item != num) {
         let imgArray = new Array();
-        imgArray[0]="04"
-        imgArray[1]="05"
-        let imgNum = Math.round(Math.random()*1)
-        let target = imgArray[imgNum]
+        imgArray[0] = "04";
+        imgArray[1] = "05";
+        let imgNum = Math.round(Math.random() * 1);
+        let target = imgArray[imgNum];
         intro.setAttribute("src", require(`@/assets/game/g${item}.png`));
-        setTimeout(() =>  intro.setAttribute("src", require(`@/assets/game/${target}.png`)), 1000);
+        setTimeout(
+          () =>
+            intro.setAttribute("src", require(`@/assets/game/${target}.png`)),
+          1000
+        );
         vm.score += 10;
       } else {
-        vm.isFalse = true
+        vm.isFalse = true;
         intro.setAttribute("src", require(`@/assets/game/g${item}.png`));
-        setTimeout(() =>  intro.setAttribute("src", require("@/assets/game/03.png")), 1000);
+        setTimeout(
+          () => intro.setAttribute("src", require("@/assets/game/03.png")),
+          1000
+        );
         const target = document.getElementById("all_btn");
         target.disabled = true;
         // 사용자 정보 DB에 저장
-        vm.save()
+        vm.save();
         setTimeout(() => this.$router.push("/rank"), 2000);
       }
     },
@@ -177,13 +198,15 @@ h5 {
   margin-top: 20px;
 }
 
-button{
-  font-size: 50px; width: 185px; margin-top: 50px
+button {
+  font-size: 50px;
+  width: 185px;
+  margin-top: 50px;
 }
 
 .v-btn-group .v-btn {
-    border-radius: 0;
-    border-color: rgba(65, 132, 234, 0.75);
-    border-width: 3px;
+  border-radius: 0;
+  border-color: rgba(65, 132, 234, 0.75);
+  border-width: 3px;
 }
 </style>
